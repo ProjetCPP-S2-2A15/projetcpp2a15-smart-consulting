9e9dcbfdc9c280847b654c88a55b6e8eb9a20d6b
AnisAbouattay <anis.bouattay@esprit.tn>
2025-04-15 00:34:48 +0100

    msg

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "connection.h"
+#include <QApplication>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+    Connection c;
+
+    bool test=c.createconnect();
+    MainWindow w;
+    w.show();
+    if(test)
+
+
+    {QMessageBox::information(nullptr, QObject::tr("Succés"),
+                                 QObject::tr("Connection successful.\n"
+                                             "Click Cancel to exit."), QMessageBox::Cancel);
+
+    }
+    return a.exec();
+
+}
